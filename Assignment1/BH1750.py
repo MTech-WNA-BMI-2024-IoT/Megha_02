37d4ef3494af4b552073f26df03f5e08b571f190
Megha <77910570+m3gha-menon@users.noreply.github.com>
2024-02-23 12:24:09 +0530

    Update BH1750.py

--- Assignment1/BH1750.py
+++ Assignment1/BH1750.py
@@ -1,15 +1,16 @@
 import time
-import adafruit_BH1750
+import adafruit_bh1750
 
 # Initialize the BH1750 sensor with the default I2C address (0x23)
-sensor = BH1750.BH1750()
+i2c = board.I2C()
+sensor = adafruit_bh1750.BH1750(i2c)
 
 # Set the measurement mode to continuous high-resolution mode
-sensor.set_mode(BH1750.BH1750.ONCE_HIRES_1)
+sensor.mode = adafruit_bh1750.BH1750.ONCE_HIRES_1
 
 while True:
     # Read the light level
-    lux = sensor.read()
+    lux = sensor.luminance
 
     # Print the light level to the terminal
     print("Light level: {:.2f} lx".format(lux))
