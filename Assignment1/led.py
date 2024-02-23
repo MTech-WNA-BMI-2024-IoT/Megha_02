7c3e6df29613207568a6faa429f73e98a9270e20
Megha <77910570+m3gha-menon@users.noreply.github.com>
2024-02-23 10:48:36 +0530

    Update led.py

--- Assignment1/led.py
+++ Assignment1/led.py
@@ -7,17 +7,16 @@ GPIO.setup(14,GPIO.OUT)
 
 # While loop
 while True:
-        # set GPIO14 pin to HIGH
-        GPIO.output(14,GPIO.HIGH)
-        # show message to Terminal
-        print "LED is ON"
-        # pause for one second
-        time.sleep(1)
+    # set GPIO14 pin to HIGH
+    GPIO.output(14,GPIO.HIGH)
+    # show message to Terminal
+    print("LED is ON")
+    # pause for one second
+    time.sleep(1)
 
-
-        # set GPIO14 pin to HIGH
-        GPIO.output(14,GPIO.LOW)
-        # show message to Terminal
-        print "LED is OFF"
-        # pause for one second
-        time.sleep(1)
+    # set GPIO14 pin to LOW
+    GPIO.output(14,GPIO.LOW)
+    # show message to Terminal
+    print("LED is OFF")
+    # pause for one second
+    time.sleep(1)
